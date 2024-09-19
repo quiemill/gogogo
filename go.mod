bec31eb91ef6313b8645a9b345f884dbc427ee37
Emiliya Sabitova <emiliasabitova46@gmail.com>
2024-09-19 22:18:50 +0500

    trying

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/quiemill/gogogo
+
+go 1.22.3
---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,11 @@
+package api
+
+type APIserver struct{}
+
+func New() *APIserver {
+	return &APIserver{}
+}
+
+func (s *APIserver) Start() error {
+	return nil
+}
