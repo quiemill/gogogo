3449b4d2199edd336da0c844293d907a0ca34e59
Emiliya Sabitova <emiliasabitova46@gmail.com>
2024-11-04 21:57:38 +0500

    start doing tg bot

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/quiemill/gogogo
-
-go 1.22.3
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/go-telegram/bot"
+	_ "github.com/go-telegram/bot/models"
+)
+
+func main() {
+	fmt.Println("Hello")
+}
