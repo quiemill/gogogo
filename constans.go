b41876fe96c52e75fe74d8afa0109a5d429d6595
Emiliya Sabitova <emiliasabitova46@gmail.com>
2024-11-05 23:44:24 +0500

    basics

---
+++ constans.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	TOKEN_NAME_IN_OS        = "just-improve-skills-bot"
+	UPDATE_CONFLICT_TIMEOUT = 60
+)
--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
-	_ "github.com/go-telegram/bot"
-	_ "github.com/go-telegram/bot/models"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var gBot *tgbotapi.BotAPI
+var gToken string
+var gChatId int64
+
 func main() {
-	fmt.Println("Hello")
+	var err error
+	//TODO: get gToken value
+	if gBot, err = tgbotapi.NewBotAPI(gToken); err != nil {
+		log.Panic(err)
+	}
+
+	gBot.Debug = true
+
+	log.Printf("Authorized on account %s", gBot.Self.UserName)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = UPDATE_CONFLICT_TIMEOUT
+
+	updates := gBot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			gBot.Send(msg)
+		}
+	}
 }
