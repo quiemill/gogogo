b0780bb5908017b760a776e3c251ec22ad93616c
Emiliya Sabitova <emiliasabitova46@gmail.com>
2024-11-26 23:40:18 +0500

    bot responds to the initial request

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -9,6 +10,23 @@ import (
 var gBot *tgbotapi.BotAPI
 var gToken string
 var gChatId int64
+
+func isStartMessage(update *tgbotapi.Update) bool {
+	return update.Message != nil && update.Message.Text == "/start"
+}
+
+func init() {
+	_ = os.Setenv(TOKEN_NAME_IN_OS, "7554516991:AAFkQhOErRU7Hx8yOQv5QoFRnWQYeZwHBJ8")
+
+	gToken = os.Getenv(TOKEN_NAME_IN_OS)
+
+	var err error
+	if gBot, err = tgbotapi.NewBotAPI(gToken); err != nil {
+		log.Panic(err)
+	}
+
+	gBot.Debug = true
+}
 
 func main() {
 	var err error
@@ -27,7 +45,7 @@ func main() {
 	updates := gBot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
+		if isStartMessage(&update) {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
