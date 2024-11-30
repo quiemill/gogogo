d5a49d2a0bf89f6a6711f0aca098afe9662f6c59
Emiliya Sabitova <emiliasabitova46@gmail.com>
2024-11-30 23:51:41 +0500

    add emojis and initial functionality

--- constans.go
+++ constans.go
@@ -3,4 +3,15 @@ package main
 const (
 	TOKEN_NAME_IN_OS        = "just-improve-skills-bot"
 	UPDATE_CONFLICT_TIMEOUT = 60
+
+	EMOJI_COIN         = "\U0001FA99"   // (coin)
+	EMOJI_SMILE        = "\U0001F642"   // 🙂
+	EMOJI_SUNGLASSES   = "\U0001F60E"   // 😎
+	EMOJI_WOW          = "\U0001F604"   // 😄
+	EMOJI_DONT_KNOW    = "\U0001F937"   // 🤷
+	EMOJI_SAD          = "\U0001F63F"   // 😿
+	EMOJI_BICEPS       = "\U0001F4AA"   // 💪
+	EMOJI_BUTTON_START = "\U000025B6  " // ▶
+	EMOJI_BUTTON_END   = "  \U000025C0" // ◀
+
 )
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -11,14 +13,12 @@ var gBot *tgbotapi.BotAPI
 var gToken string
 var gChatId int64
 
-func isStartMessage(update *tgbotapi.Update) bool {
-	return update.Message != nil && update.Message.Text == "/start"
-}
-
 func init() {
 	_ = os.Setenv(TOKEN_NAME_IN_OS, "7554516991:AAFkQhOErRU7Hx8yOQv5QoFRnWQYeZwHBJ8")
 
-	gToken = os.Getenv(TOKEN_NAME_IN_OS)
+	if gToken = os.Getenv(TOKEN_NAME_IN_OS); gToken == "" {
+		panic(fmt.Errorf("failed to load environment variable %s", TOKEN_NAME_IN_OS))
+	}
 
 	var err error
 	if gBot, err = tgbotapi.NewBotAPI(gToken); err != nil {
@@ -26,6 +26,32 @@ func init() {
 	}
 
 	gBot.Debug = true
+}
+
+func isStartMessage(update *tgbotapi.Update) bool {
+	return update.Message != nil && update.Message.Text == "/start"
+}
+
+func delay(seconds uint8) {
+	time.Sleep(time.Second * time.Duration(seconds))
+}
+
+func printSystemMessageWithDelay(delayInSec uint8, message string) {
+	gBot.Send(tgbotapi.NewMessage(gChatId, message))
+	time.Sleep(time.Second * time.Duration(delayInSec))
+}
+func printIntro(update *tgbotapi.Update) {
+	printSystemMessageWithDelay(2, "Hello! "+EMOJI_SUNGLASSES)
+	printSystemMessageWithDelay(7, "There are numerous beneficial actions that, by performing regularly, we improve the quality of our life. However, often it's more fun, easier, or tastier to do something harmful. Isn't that so?")
+	printSystemMessageWithDelay(7, "With greater likelihood, we'll prefer to get lost in YouTube Shorts instead of an English lesson, buy M&M's instead of vegetables, or lie in bed instead of doing yoga.")
+	printSystemMessageWithDelay(1, EMOJI_SAD)
+	printSystemMessageWithDelay(10, "Everyone has played at least one game where you need to level up a character, making them stronger, smarter, or more beautiful. It's enjoyable because each action brings results. In real life, though, systematic actions over time start to become noticeable. Let's change that, shall we?")
+	printSystemMessageWithDelay(1, EMOJI_SMILE)
+	printSystemMessageWithDelay(14, `Before you are two tables: "Useful Activities" and "Rewards". The first table lists simple short activities, and for completing each of them, you'll earn the specified amount of coins. In the second table, you'll see a list of activities you can only do after paying for them with coins earned in the previous step.`)
+	printSystemMessageWithDelay(1, EMOJI_COIN)
+	printSystemMessageWithDelay(10, `For example, you spend half an hour doing yoga, for which you get 2 coins. After that, you have 2 hours of programming study, for which you get 8 coins. Now you can watch 1 episode of "Interns" and break even. It's that simple!`)
+	printSystemMessageWithDelay(6, `Mark completed useful activities to not lose your coins. And don't forget to "purchase" the reward before actually doing it.`)
+
 }
 
 func main() {
@@ -47,6 +73,8 @@ func main() {
 	for update := range updates {
 		if isStartMessage(&update) {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			gChatId = update.Message.Chat.ID
+			printIntro(&update)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
